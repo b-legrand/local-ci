647154859eadbffa45713e0abc96167603a72eba
Benjamin Legrand <benjamin.legrand.externe@capgemini.com>
2021-10-28 19:41:56 +0200

    feat(): add new pages

--- apps/apm-ssr/src/app/app-routing.module.ts
+++ apps/apm-ssr/src/app/app-routing.module.ts
@@ -9,21 +9,51 @@ const routes: Routes = [];
       [
         {
           path: '',
-          pathMatch: "full",
-          loadChildren: () =>
-            import('./pages/home-page/home-page.module').then(
-              (m) => m.HomePageModule
-            ),
+          pathMatch: 'full',
+          children: [
+            {
+              path: '',
+              loadChildren: () =>
+                import('./pages/home-page/home-page.module').then(
+                  (m) => m.HomePageModule
+                ),
+            },
+            {
+              path: 'demo',
+              loadChildren: () =>
+                import('./pages/demo-page/demo-page.module').then(
+                  (m) => m.DemoPageModule
+                ),
+            },
+            {
+              path: 'login',
+              loadChildren: () =>
+                import('./pages/login-page/login-page.module').then(
+                  (m) => m.LoginPageModule
+                ),
+            },
+            {
+              path: 'consent',
+              loadChildren: () =>
+                import('./pages/consent-page/consent-page.module').then(
+                  (m) => m.ConsentPageModule
+                ),
+            },
+          ],
         },
         {
-          path: 'demo',
+          path: 'error',
           loadChildren: () =>
-            import('./pages/demo-page/demo-page.module').then(
-              (m) => m.DemoPageModule
+            import('./pages/error-page/error-page.module').then(
+              (m) => m.ErrorPageModule
             ),
         },
+        { path: '**', redirectTo: 'error' },
       ],
       {
+        anchorScrolling: 'enabled',
+        useHash: false,
+        enableTracing: false,
         initialNavigation: 'enabled',
       }
     ),
